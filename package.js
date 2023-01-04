52c35778f0af1955fb64647cca9b909867e394ab
Themadpunter <106832853+familycomicsstudios@users.noreply.github.com>
2023-01-04 16:49:48 -0600

    Update package.js

--- package.js
+++ package.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 // You should use require('@turbowarp/packager') instead
 // We use a strange require() in this demo because we use this to test the API internally
-const Packager = require('../dist/packager');
+const Packager = require('packager');
 
 const run = async () => {
-  const projectData = fs.readFileSync(path.join(__dirname, '..', 'static', 'example.sb3'));
+  const projectData = fs.readFileSync(path.join('pMod.sb3'));
 
   const loadedProject = await Packager.loadProject(projectData);
 
